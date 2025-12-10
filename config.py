60bb235d3aec030f318fc6ba9148a63544416e54
ciminh <113219891+ciminh@users.noreply.github.com>
2025-12-10 13:55:57 +0800

    Change database host and S3 bucket in config
    Updated database host and S3 bucket name.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cb26ggko88uw.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "teowcheeminh-employee"
 customregion = "us-east-1"
 
